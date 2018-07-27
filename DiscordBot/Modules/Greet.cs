7b4bb6e23579b89dc654c93284541198be4255b3
Ege TUNÇÖZ <etuncoz@veripark.com>
2018-07-27 17:21:06 +0300

    Add project files.

---
+++ DiscordBot/Modules/Greet.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Greet : ModuleBase<SocketCommandContext>
+    {
+        [Command("salut")]
+        public async Task GreetAsync()
+        {
+            await ReplyAsync("Selam canım");
+        }
+
+    }
+}
