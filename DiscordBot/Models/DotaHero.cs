df8a0f88f217b9e205971f975f2a54ae7015d6cf
etuncoz <tuncozege@gmail.com>
2020-10-28 11:05:15 +0300

    added dota random commands

---
+++ DiscordBot/Models/DotaHero.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Models
+{
+    public class DotaHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [JsonProperty("localized_name")]
+        public string LocalizedName { get; set; }
+        [JsonProperty("primary_attr")]
+        public string PrimaryAttribute { get; set; }
+        [JsonProperty("attack_type")]
+        public string AttackType { get; set; }
+        public List<string> Roles { get; set; }
+        public int Legs { get; set; }
+    }
+}
